01872782dc5f73028245194085e85bddfe10902f
Saiyaf <saiyaflrf@gmail.com>
2022-10-22 09:49:37 +0530

    Initialization

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable indent */
+module.exports = {
+    purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
