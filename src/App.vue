c61c25c07b3883d2e93010f70c35d63995b938a3
Saiyaf <saiyaflrf@gmail.com>
2022-11-29 08:42:23 +0530

    Added-Buttons

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
   <AuthModal />
 
   <!-- Introduction -->
-  <section class="mb-8 py-20 text-white text-center relative">
-    <div class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-      style="background-image: url(assets/img/header.png)"></div>
+  <section class="mb-8 py-32 text-white text-center relative">
+    <div class="absolute inset-0 w-full h-full introduction-bg bg-auto md:bg-cover"
+    style="background-image: url(assets/img/header.jpg)"></div>
     <div class="container mx-auto">
-      <div class="text-black main-header-content">
+      <div class="text-white main-header-content">
         <h1 class="font-bold text-5xl mb-5">Welcome To Swift Tail</h1>
-        <p class="w-full md:w-8/12 mx-auto">
+        <p class="w-full md:w-8/12 mx-auto text-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Phasellus et dolor mollis, congue augue non, venenatis elit.
           Nunc justo eros, suscipit ac aliquet imperdiet, venenatis et
@@ -20,14 +20,15 @@
         </p>
       </div>
     </div>
-
-    <img class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-      src="assets/img/header.png" />
   </section>
 
-  <!-- Main Content -->
+  <!-- Options -->
   <section class="container mx-auto">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+    <div class="bg-white rounded border border-gray-200 flex flex-row space-x-7 md:space-x-32 p-7 justify-center text-xl font-semibold">
+      <div><button class="bg-gradient-to-r from-green-400 to-green-200 rounded-lg border border-gray-200 h-auto md:h-16 p-2">Train Schedule</button></div>
+      <div><button class="bg-gradient-to-r from-green-400 to-green-200 rounded-lg border border-gray-200 h-auto md:h-16 p-2">Train Reservation</button></div>
+      <div><button class="bg-gradient-to-r from-green-400 to-green-200 rounded-lg border border-gray-200 h-auto md:h-16 p-2">Train Reservation Cancellation</button></div>
+      <div><button class="bg-gradient-to-r from-green-400 to-green-200 rounded-lg border border-gray-200 h-auto md:h-16 p-2">Hotel Booking</button></div>
     </div>
   </section>
 
